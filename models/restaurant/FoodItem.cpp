d88a2f4a63566641d3abca27755da1cc064ec3de
SaraBagheban <sarabaghebane@gmail.com>
2026-07-14 07:27:05 +0330

    Add FoodItem class

---
+++ models/restaurant/FoodItem.cpp
@@ -0,0 +1,45 @@
+#include "FoodItem.h"
+
+#include <iostream>
+
+FoodItem::FoodItem() : MenuItem(), cookTime(0) {
+}
+
+FoodItem::FoodItem(int id, const std::string& name,
+                   const std::string& description,
+                   double basePrice, bool available,
+                   int cookTime)
+    : MenuItem(id, name, description, basePrice, available),
+      cookTime(cookTime) {
+}
+
+
+
+int FoodItem::getCookTime() const {
+    return cookTime;
+}
+
+void FoodItem::setCookTime(int cookTime) {
+    this->cookTime = cookTime;
+}
+
+
+double FoodItem::calculatePrice() const {
+    double extraCookFactor = (cookTime - 15) / 15.0;
+
+    if (extraCookFactor < 0) {
+        extraCookFactor = 0;
+    }
+
+    const double pricePer15Minutes = 0.05;
+
+    return getBasePrice() * (1 + extraCookFactor * pricePer15Minutes);
+}
+
+void FoodItem::showDetails() const {
+    std::cout << "Name: " << getName() << std::endl;
+    std::cout << "Description: " << getDescription() << std::endl;
+    std::cout << "Cook Time: " << cookTime << " min" << std::endl;
+    std::cout << "Price: " << calculatePrice() << std::endl;
+    std::cout << "Available: " << (getAvailable() ? "Yes" : "No") << std::endl;
+}
---
+++ models/restaurant/FoodItem.h
@@ -0,0 +1,25 @@
+#ifndef FOODITEM_H
+#define FOODITEM_H
+
+#include "MenuItem.h"
+
+class FoodItem : public MenuItem {
+private:
+    int cookTime;
+
+public:
+    FoodItem();
+    FoodItem(int id, const std::string& name,
+             const std::string& description,
+             double basePrice, bool available,
+             int cookTime);
+
+             
+    int getCookTime() const;
+    void setCookTime(int cookTime);
+
+    double calculatePrice() const override;
+    void showDetails() const override;
+};
+
+#endif

89b16d79e0750a8a556f3d4295783ef5f3e48587
SaraBagheban <sarabaghebane@gmail.com>
2026-07-15 09:00:34 +0330

    Update Customer to use membership levels

--- models/users/Customer.cpp
+++ models/users/Customer.cpp
@@ -2,11 +2,78 @@
 
 #include <iostream>
 
-Customer::Customer() : User() {
+Customer::Customer() : User(), membershipLevel(nullptr),
+      loyaltyPoints(0), successfulOrders(0), couponCount(0) {
 }
 
 Customer::Customer(int id, const std::string& name, const std::string& phone)
-    : User(id, name, phone) {
+    : User(id, name, phone), membershipLevel(nullptr),
+      loyaltyPoints(0), successfulOrders(0), couponCount(0) {
+}
+
+Customer::Customer(int id, const std::string& name, const std::string& phone,
+                   MembershipLevel* membershipLevel, int loyaltyPoints,
+                   int successfulOrders, int couponCount)
+    : User(id, name, phone), membershipLevel(membershipLevel),
+      loyaltyPoints(loyaltyPoints), successfulOrders(successfulOrders),
+      couponCount(couponCount) {
+}
+
+Customer::~Customer() {
+}
+
+
+
+MembershipLevel* Customer::getMembershipLevel() const {
+    return membershipLevel;
+}
+
+int Customer::getLoyaltyPoints() const {
+    return loyaltyPoints;
+}
+
+int Customer::getSuccessfulOrders() const {
+    return successfulOrders;
+}
+
+int Customer::getCouponCount() const {
+    return couponCount;
+}
+
+std::vector<Badge>& Customer::getBadges() {
+    return badges;
+}
+
+std::vector<LevelHistory>& Customer::getLevelHistory() {
+    return levelHistory;
+}
+
+
+
+void Customer::setMembershipLevel(MembershipLevel* membershipLevel) {
+    this->membershipLevel = membershipLevel;
+}
+
+void Customer::setLoyaltyPoints(int loyaltyPoints) {
+    this->loyaltyPoints = loyaltyPoints;
+}
+
+void Customer::setSuccessfulOrders(int successfulOrders) {
+    this->successfulOrders = successfulOrders;
+}
+
+void Customer::setCouponCount(int couponCount) {
+    this->couponCount = couponCount;
+}
+
+
+
+void Customer::addBadge(const Badge& badge) {
+    badges.push_back(badge);
+}
+
+void Customer::addLevelHistory(const LevelHistory& history) {
+    levelHistory.push_back(history);
 }
 
 void Customer::showMenu() {
--- models/users/Customer.h
+++ models/users/Customer.h
@@ -2,11 +2,65 @@
 #define CUSTOMER_H
 
 #include "User.h"
+#include "../membership/MembershipLevel.h"
+#include "../membership/Badge.h"
+#include "../membership/LevelHistory.h"
+
+#include <vector>
 
 class Customer : public User {
+private:
+    MembershipLevel* membershipLevel;
+
+    int loyaltyPoints;
+    int successfulOrders;
+    int couponCount;
+
+    std::vector<Badge> badges;
+    std::vector<LevelHistory> levelHistory;
+
 public:
     Customer();
+
     Customer(int id, const std::string& name, const std::string& phone);
+
+    Customer(int id, const std::string& name,
+             const std::string& phone,
+             MembershipLevel* membershipLevel,
+             int loyaltyPoints, int successfulOrders,
+             int couponCount);
+
+    ~Customer();
+
+
+
+    MembershipLevel* getMembershipLevel() const;
+
+    int getLoyaltyPoints() const;
+
+    int getSuccessfulOrders() const;
+
+    int getCouponCount() const;
+
+    std::vector<Badge>& getBadges();
+
+    std::vector<LevelHistory>& getLevelHistory();
+
+
+
+    void setMembershipLevel(MembershipLevel* membershipLevel);
+
+    void setLoyaltyPoints(int loyaltyPoints);
+
+    void setSuccessfulOrders(int successfulOrders);
+
+    void setCouponCount(int couponCount);
+
+
+
+    void addBadge(const Badge& badge);
+
+    void addLevelHistory(const LevelHistory& history);
 
     void showMenu() override;
 };
